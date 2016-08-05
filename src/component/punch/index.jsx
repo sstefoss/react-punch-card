a89db96dd515b74c60add547810ae32d53428257
stef <stef.orovas@gmail.com>
2016-08-06 00:39:37 +0300

    punch passes components to children

--- src/component/punch/index.jsx
+++ src/component/punch/index.jsx
@@ -4,18 +4,22 @@ import './style';
 export default class Punch extends React.Component {
 
   static propTypes = {
-    data: React.PropTypes.number.isRequired
+    data: React.PropTypes.number
   }
 
   render() {
     const {data, children} = this.props;
     const size = data * 10;
+    const childrenWithProps = React.Children.map(children, child => React.cloneElement(child, {
+        data: data
+      })
+    );
     return (
       <div className='punch'>
         <div
           className='punch--point'
           style={{width: `${size}%`, height: `${size}%`}}>
-          {children}
+          {childrenWithProps}
         </div>
       </div>
     );
